270233dfa6512c5fe7c1a9f0d9a6989fdb42541f
Rebeca Jimenez <rebeca.jimenez.2635@gmail.com>
2024-05-28 13:22:16 -0600

    Se agregan validaciones a campos Nombre y Cantidad

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
 // El código va aquí -> 
+
+//Definiendo elementos
+let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+
+let txtNombre = document.getElementById("Name");
+let txtNumber =document.getElementById("Number");
+
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+
+
+//Función 
+function validarcantidad(){
+    if(txtNumber.value.length==0){
+        return false;
+    }
+        return true;
+}
+
+//BOTÓN AGREGAR
+btnAgregar.addEventListener("click", function(event){
+
+    event.preventDefault();
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtNombre.style.border="";
+    txtNumber.style.border="";
+
+    if(txtNombre.value.length<3){
+         alertValidacionesTexto.innerHTML = "El <strong>Nombre</strong> no es correcto<br/>";
+         alertValidaciones.style.display = "block";
+         txtNombre.style.border = "solid red medium";
+    }
+
+    if(!validarcantidad()){
+        alertValidacionesTexto.innerHTML+="El <strong>Numero</strong>no es correcto";
+        alertValidaciones.style.display="block";
+        txtNumber.style.border="solid red medium";
+    }
+
+});
+
+//BOTÓN LIMPIAR remover información capturada
+btnClear.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    txtNombre.value = "";     //Limpiar campos
+    txtNumber.value ="";
+});
